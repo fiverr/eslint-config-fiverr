0508955b12e0be004a81dc0dc4d070f3c4d2e612
KobiS2 <70258939+KobiS2@users.noreply.github.com>
2020-08-26 17:25:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ module.exports = {
     extends: [
         './rules/base',
         './rules/es6',
-        './rules/react'
+        './rules/react',
+        './rules/dev',
+		'./rules/jest',
+        './rules/npm',
+		'./rules/start'
     ].map(require.resolve),
     rules: {}
 };
