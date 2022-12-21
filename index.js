42ae048292f27ca502860d85ed289437118f0717
Moshe Katz <moshe.katz@hey.com>
2022-12-21 16:52:28 +0200

    no require.resolve

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const baseRules = require('./rules/base');
+const es6Rules = require('./rules/es6');
+const reactRules = require('./rules/react');
+
+
 module.exports = {
     extends: [
-        './rules/base',
-        './rules/es6',
-        './rules/react',
-    ]
-    .map(require.resolve)
-    .push('prettier'),
+        baseRules,
+        es6Rules,
+        reactRules,
+        'prettier'
+    ],
     rules: {}
 };
