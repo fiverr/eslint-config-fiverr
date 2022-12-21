73ef96eb7be8f4ebed14778c3117918f33d2f49b
Moshe Katz <moshe.katz@hey.com>
2022-12-21 16:53:00 +0200

    another one

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-const baseRules = require('./rules/base');
-const es6Rules = require('./rules/es6');
-const reactRules = require('./rules/react');
-
-
 module.exports = {
     extends: [
-        baseRules,
-        es6Rules,
-        reactRules,
-        'prettier'
-    ],
+        './rules/base',
+        './rules/es6',
+        './rules/react',
+        
+    ]
+    .concat(['prettier']),
     rules: {}
 };
